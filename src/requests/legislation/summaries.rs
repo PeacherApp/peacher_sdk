6e56f45a9d652b46fffe14d6b306da0e1afb9af7
Daniel Gallups <44790295+dsgallups@users.noreply.github.com>
2026-02-13 08:21:55 -0500

    Trust system (#9)
    * add summary requests and views
    
    * refactor: pages start at 0

--- src/requests/legislation/summaries.rs
+++ src/requests/legislation/summaries.rs
@@ -1,8 +1,33 @@
+use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
 
 /// Request to create a new piece of legislation
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-pub struct CreateLegislationSummaryRequest {
+pub struct CreateSummaryRequest {
     pub content: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub struct SummaryView {
+    pub id: i32,
+    pub created_at: DateTime<FixedOffset>,
+    pub updated_at: DateTime<FixedOffset>,
+    pub content: String,
+    pub visibility: Visibility,
+    pub author: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub struct ListSummariesResponse {
+    pub summaries: Vec<SummaryView>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub enum Visibility {
+    NotVisible,
+    Public,
 }
