84a5ea2679b3e526eba8724fedc577220ee83fa9
Daniel Gallups <dsgallups@protonmail.com>
2026-02-16 07:53:11 -0500

    update summary params

--- src/sdk/legislation/summaries.rs
+++ src/sdk/legislation/summaries.rs
@@ -1,6 +1,7 @@
 use crate::{paginated, prelude::*};
 use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
+use strum::{Display, EnumString};
 use uuid::Uuid;
 
 /// Request to create a new summary for a piece of legislation
@@ -26,22 +27,43 @@ pub struct SummaryView {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-pub struct ListSummariesResponse {
-    pub summaries: Vec<SummaryView>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 pub enum Visibility {
     NotVisible,
     Public,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-pub struct SummaryParams {
+#[cfg_attr(feature = "utoipa", derive(utoipa::IntoParams))]
+#[cfg_attr(feature = "utoipa", into_params(parameter_in = Query))]
+pub struct ModeratorSummaryParams {
     pub page: Option<u64>,
     pub page_size: Option<u64>,
+    /// note that on the legislation/{id}/summaries route,
+    /// this value is overwritten.
+    pub legislation_id: Option<i32>,
+    /// Note that this parameter is overwritten if the viewer does not have sufficient privileges
+    pub visibility: Option<Visibility>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::IntoParams))]
+#[cfg_attr(feature = "utoipa", into_params(parameter_in = Query))]
+pub struct SummaryParams {
+    pub order_by: SummaryOrder,
+    pub order: Ordering,
+    pub page: Option<u64>,
+    pub page_size: Option<u64>,
+}
+
+#[derive(
+    Default, Clone, Copy, EnumString, Display, Debug, PartialEq, Eq, Serialize, Deserialize,
+)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+#[strum(serialize_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
+pub enum SummaryOrder {
+    #[default]
+    Weight,
 }
 
 paginated!(SummaryParams);
