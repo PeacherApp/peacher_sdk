9c35b0ac4305908832e9b342ff907805d4fad100
Daniel Gallups <dsgallups@protonmail.com>
2026-02-22 06:06:04 -0500

    first impl of notification views

--- src/sdk/notifications.rs
+++ src/sdk/notifications.rs
@@ -1,5 +1,58 @@
+use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
+use strum::{Display, EnumString};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-pub struct NotificationPreferencesResponse {}
+pub struct NotificationPreferencesResponse {
+    pub preferences: Vec<NotificationPreference>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub struct NotificationPreference {
+    pub id: i32,
+    pub created_at: DateTime<FixedOffset>,
+    pub updated_at: DateTime<FixedOffset>,
+    pub sink: NotificationSink,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+#[serde(tag = "type", content = "value", rename_all = "snake_case")]
+pub enum NotificationSink {
+    InApp(InAppNotificationPreferences),
+    Email(EmailNotificationPreferences),
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Display, EnumString)]
+#[serde(rename_all = "snake_case")]
+#[strum(serialize_all = "snake_case")]
+pub enum PreferenceType {
+    InApp,
+    Email,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub struct InAppNotificationPreferences {
+    enabled: bool,
+}
+impl Default for InAppNotificationPreferences {
+    fn default() -> Self {
+        Self { enabled: true }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub struct EmailNotificationPreferences {
+    enabled: bool,
+}
+
+#[expect(clippy::derivable_impls)]
+impl Default for EmailNotificationPreferences {
+    fn default() -> Self {
+        Self { enabled: false }
+    }
+}
