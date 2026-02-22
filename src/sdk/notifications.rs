3e44f223baf491ad963b2ca7fdde4001ca4dd857
Daniel Gallups <dsgallups@protonmail.com>
2026-02-22 06:29:26 -0500

    use generics over enum for preferences

--- src/sdk/notifications.rs
+++ src/sdk/notifications.rs
@@ -5,24 +5,24 @@ use strum::{Display, EnumString};
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 pub struct NotificationPreferencesResponse {
-    pub preferences: Vec<NotificationPreference>,
+    pub email: NotificationPreference<EmailNotificationPreferences>,
+    pub in_app: NotificationPreference<InAppNotificationPreferences>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-pub struct NotificationPreference {
+pub struct NotificationPreferenceMeta {
     pub id: i32,
     pub created_at: DateTime<FixedOffset>,
     pub updated_at: DateTime<FixedOffset>,
-    pub sink: NotificationSink,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-#[serde(tag = "type", content = "value", rename_all = "snake_case")]
-pub enum NotificationSink {
-    InApp(InAppNotificationPreferences),
-    Email(EmailNotificationPreferences),
+pub struct NotificationPreference<P> {
+    /// if this is None, then the preference has never been recorded.
+    pub meta: Option<NotificationPreferenceMeta>,
+    pub preference: P,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Display, EnumString)]
