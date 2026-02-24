3082b30c7614a2508c1532e43162f2077eed3770
Daniel Gallups <dsgallups@protonmail.com>
2026-02-24 08:53:47 -0500

    remove some stuff from reportreviewed

--- src/sdk/notifications/reports.rs
+++ src/sdk/notifications/reports.rs
@@ -1,12 +1,11 @@
 use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
 
-use crate::sdk::{MemberView, ReportDetails, ReviewStatus};
+use crate::sdk::{MemberView, ReviewStatus};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 pub struct ReportReviewed {
-    pub report_id: i32,
     pub reviewed_at: DateTime<FixedOffset>,
     pub reviewer: MemberView,
     pub message: String,
@@ -19,5 +18,4 @@ pub struct ReportCreated {
     pub created_at: DateTime<FixedOffset>,
     pub reporter: MemberView,
     pub report_reason: String,
-    pub details: ReportDetails,
 }
