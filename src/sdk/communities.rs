ae06bad99fec4bf41e12f388b818564515b238e7
Daniel Gallups <dsgallups@protonmail.com>
2026-02-27 05:52:59 -0500

    update district views

--- src/sdk/communities.rs
+++ src/sdk/communities.rs
@@ -15,6 +15,75 @@ pub struct CommunityView {
     pub description: Option<String>,
     pub icon_url: Option<String>,
     pub banner_url: Option<String>,
+    pub created_at: DateTime<FixedOffset>,
+    pub updated_at: DateTime<FixedOffset>,
+}
+impl CommunityView {
+    pub fn with_count(self, member_count: u64) -> CommunityViewWithCount {
+        CommunityViewWithCount {
+            id: self.id,
+            name: self.name,
+            description: self.description,
+            icon_url: self.icon_url,
+            banner_url: self.banner_url,
+            member_count,
+            created_at: self.created_at,
+        }
+    }
+    pub fn with_join_date(self, join_date: DateTime<FixedOffset>) -> CommunityViewWithJoinDate {
+        CommunityViewWithJoinDate {
+            id: self.id,
+            name: self.name,
+            description: self.description,
+            icon_url: self.icon_url,
+            banner_url: self.banner_url,
+            join_date,
+            created_at: self.created_at,
+        }
+    }
+    pub fn with_details(
+        self,
+        rules: Option<String>,
+        member_count: u64,
+        created_by: MemberView,
+        districts: impl IntoIterator<Item = DistrictView>,
+    ) -> CommunityDetailView {
+        CommunityDetailView {
+            id: self.id,
+            name: self.name,
+            rules,
+            updated_at: self.updated_at,
+            description: self.description,
+            icon_url: self.icon_url,
+            banner_url: self.banner_url,
+            member_count,
+            created_by,
+            districts: districts.into_iter().collect(),
+            created_at: self.created_at,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub struct CommunityViewWithJoinDate {
+    pub id: i32,
+    pub name: String,
+    pub description: Option<String>,
+    pub icon_url: Option<String>,
+    pub banner_url: Option<String>,
+    pub join_date: DateTime<FixedOffset>,
+    pub created_at: DateTime<FixedOffset>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub struct CommunityViewWithCount {
+    pub id: i32,
+    pub name: String,
+    pub description: Option<String>,
+    pub icon_url: Option<String>,
+    pub banner_url: Option<String>,
     pub member_count: u64,
     pub created_at: DateTime<FixedOffset>,
 }
@@ -30,17 +99,9 @@ pub struct CommunityDetailView {
     pub banner_url: Option<String>,
     pub member_count: u64,
     pub created_by: MemberView,
-    pub districts: Vec<CommunityDistrictView>,
+    pub districts: Vec<DistrictView>,
     pub created_at: DateTime<FixedOffset>,
     pub updated_at: DateTime<FixedOffset>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-pub struct CommunityDistrictView {
-    pub map_id: i32,
-    pub district_id: i32,
-    pub district_name: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
