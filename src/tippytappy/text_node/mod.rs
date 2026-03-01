5944f8071f04d8e7b4c3db01187519ba75b8bae1
Daniel Gallups <dsgallups@protonmail.com>
2026-03-01 16:50:18 -0500

    chore: delete dead code

--- src/tippytappy/text_node/mod.rs
+++ src/tippytappy/text_node/mod.rs
@@ -6,42 +6,6 @@ pub use compiled::*;
 
 use serde::{Deserialize, Serialize};
 
-// #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-// #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-// #[serde(rename_all = "camelCase")]
-// pub struct TextNode<S: State> {
-//     #[serde(flatten)]
-//     inner: S::TextNode,
-// }
-
-// impl TextNode<View> {
-//     pub fn compile(self, carriage: &mut CompileCarriage) -> TextNode<Compiled> {
-//         let new_inner = self.inner.compile(carriage);
-//         TextNode { inner: new_inner }
-//     }
-
-//     #[allow(clippy::self_named_constructors)]
-//     pub fn text_node(text: Text) -> Self {
-//         Self {
-//             inner: TextNodeView::Text(text),
-//         }
-//     }
-//     pub fn text(&self) -> &str {
-//         self.inner.text()
-//     }
-//     pub fn inner(&self) -> &TextNodeView {
-//         &self.inner
-//     }
-// }
-
-// impl TextNode<Compiled> {
-//     pub fn into_view(self, relationships: &ContentRelationships) -> TextNode<View> {
-//         let new_inner = self.inner.into_view(relationships);
-
-//         TextNode { inner: new_inner }
-//     }
-// }
-
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 #[serde(rename_all = "camelCase")]
