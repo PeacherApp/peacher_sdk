af405ca3008fb627e5784f4883bb3bd1e203dcb2
Daniel Gallups <dsgallups@protonmail.com>
2026-03-02 08:36:50 -0500

    fix: toschema impl for documentview

--- src/tippytappy/document_view.rs
+++ src/tippytappy/document_view.rs
@@ -11,10 +11,50 @@ impl State for View {
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
 #[serde(tag = "type", rename = "doc")]
 pub struct DocumentView {
     content: Vec<Node<View>>,
+}
+
+/// Need to manually implement this since
+///
+/// utoipa does not generate the correct type for structs that are internally tagged.
+#[cfg(feature = "utoipa")]
+impl utoipa::PartialSchema for DocumentView {
+    fn schema() -> utoipa::openapi::RefOr<utoipa::openapi::schema::Schema> {
+        use utoipa::openapi::schema::Type;
+        utoipa::openapi::ObjectBuilder::new()
+            .property(
+                "type",
+                utoipa::openapi::ObjectBuilder::new()
+                    .schema_type(Type::String)
+                    .enum_values::<_, &str>(Some(["doc"])),
+            )
+            .required("type")
+            .property(
+                "content",
+                utoipa::openapi::schema::ArrayBuilder::new()
+                    .items(utoipa::openapi::Ref::from_schema_name("Node_View")),
+            )
+            .required("content")
+            .into()
+    }
+}
+
+#[cfg(feature = "utoipa")]
+impl utoipa::ToSchema for DocumentView {
+    fn name() -> std::borrow::Cow<'static, str> {
+        "DocumentView".into()
+    }
+
+    fn schemas(
+        schemas: &mut Vec<(
+            String,
+            utoipa::openapi::RefOr<utoipa::openapi::schema::Schema>,
+        )>,
+    ) {
+        <Node<View> as utoipa::ToSchema>::schemas(schemas);
+    }
 }
 
 impl DocumentView {
