4e9e723fb17ffec7d27d02df994f22ee99efb950
Daniel Gallups <dsgallups@protonmail.com>
2026-03-31 15:31:53 -0400

    update transport message structs

--- src/webtransport/mod.rs
+++ src/webtransport/mod.rs
@@ -1,7 +1,24 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
-pub enum WebTransportMsg {
-    Say(String),
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type", content = "data")]
+pub enum ClientWebTransportMsg {
+    Join(i32),
+    Leave,
+    Say { text: String },
     Nothing,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type", content = "data")]
+pub enum ServerWebTransportMsg {
+    Message { from: i32, content: String },
+    Error(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
+pub struct WebTransportInfo {
+    pub url: String,
+    pub cert_hash: Vec<u8>,
 }
