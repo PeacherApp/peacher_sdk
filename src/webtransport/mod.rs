f1e352a882be8324f3cbf455f56d283b815cdc30
Daniel Gallups <dsgallups@protonmail.com>
2026-03-31 16:34:10 -0400

    add encoder/decoder for wtransport messages

--- src/webtransport/mod.rs
+++ src/webtransport/mod.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -10,6 +11,26 @@ pub enum ClientWebTransportMsg {
     Say { text: String },
     Nothing,
 }
+impl ClientWebTransportMsg {
+    /// this method allocated an internal vector and then extends the passed in buffer.
+    ///
+    /// This isn't fantastic. It's just a quick impl.
+    ///
+    /// Does not clear the buffer. extends it.
+    pub fn append_into(&self, buf: &mut Vec<u8>) {
+        let payload = postcard::to_allocvec(self).expect("serialization failed");
+
+        let needed_cap = 4 + payload.len();
+
+        if buf.capacity() < needed_cap {
+            let additional_to_reserve = needed_cap - buf.capacity();
+            _ = buf.try_reserve(additional_to_reserve);
+        }
+
+        buf.extend_from_slice(&(payload.len() as u32).to_be_bytes());
+        buf.extend_from_slice(&payload);
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[cfg_attr(feature = "web", derive(tsify::Tsify))]
@@ -18,6 +39,16 @@ pub enum ClientWebTransportMsg {
 pub enum ServerWebTransportMsg {
     Message { from: i32, content: String },
     Error(String),
+}
+
+impl ServerWebTransportMsg {
+    pub fn decode(buf: &[u8]) -> anyhow::Result<Self> {
+        let payload = buf.get(4..).context("Buffer too short")?;
+
+        let this = postcard::from_bytes(payload)?;
+
+        Ok(this)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
